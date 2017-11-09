97eef6443f9fe98d438b16c08670b5236386a240
Jacob Pernell <jacob.pernell@gmail.com>
2017-11-08 22:16:04 -0800

    It reads the file, woo! But its printing the html code versus rendering it. Will fix this next.

--- server.js
+++ server.js
@@ -2,10 +2,18 @@
 
 (function() {
 	const http = require('http');
+	const fs = require('fs');
 
 	http.createServer((request, response) => {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.end(`Hello World! It's ${new Date().toISOString()}\n`);
+		fs.readFile('./dist/index.html', function(error, content) {
+			if (error) {
+				response.writeHead(500);
+				response.end();
+			} else {
+				response.writeHead(200, {'Content-Type': 'text/plain'});
+				response.end(content, 'utf-8');
+			}
+		})
 	}).listen(8080);
 
 	console.log('Server running on port 8080');
