f750d671b26a974ad78ee715729a2daf7680d391
Jacob Pernell <jacob.pernell@gmail.com>
2017-11-10 16:41:03 -0800

    Broken, but one step closer to getting the other file types to be read properly. Need to fix file directory structure

--- server.js
+++ server.js
@@ -3,17 +3,44 @@
 (function() {
 	const http = require('http');
 	const fs = require('fs');
+	const path = require('path');
 
 	http.createServer((request, response) => {
-		fs.readFile('./dist/index.html', function(error, content) {
-			if (error) {
-				response.writeHead(500);
-				response.end('Error');
-			} else {
-				response.writeHead(200, {'Content-Type': 'text/html'});
-				response.end(content, 'utf-8');
+		
+		let filePath = '.' + request.url;
+		if (filePath === './') {
+			filePath = './dist/index.html';
+		}
+
+		let extentionType = path.extname(filePath);
+		let contentType = 'text/html';
+
+		switch (extentionType) {
+			case '.js':
+				contentType = 'text/javascript';
+				break;
+			case '.css':
+				contentType = 'text/css';
+				break;
+		}
+
+		//path.exists(filePath, function(exists) {
+			//if (exists) {
+				fs.readFile(filePath, function(error, content) {
+					if (error) {
+						response.writeHead(500);
+						response.end();
+					} else {
+						response.writeHead(200, { 'Content-Type': contentType });
+						response.end(content, 'utf-8');
+					}
+				});
+			/*} else {
+				response.writeHead(404);
+				response.end();
 			}
-		})
+		});*/
+
 	}).listen(8080);
 
 	console.log('Server running on port 8080');
