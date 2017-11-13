b2cbfa009c0658b020ab92628729777c2a1398d4
Jacob Pernell <jacob.pernell@gmail.com>
2017-11-13 04:17:21 -0800

    Starting to build out the basis for the stringify function

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,4 +2,12 @@
 
 (function() {
 	console.log('JS loaded!');
+
+	const inputText = document.querySelector('#input_text');
+	const inputTextButton = document.querySelector('#input_text_button');
+	inputTextButton.addEventListener("click", stringifyInput(inputText));
+
+	const stringifyInput = input => {
+		console.log(input);
+	};
 })();
