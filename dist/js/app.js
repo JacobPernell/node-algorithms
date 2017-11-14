ac3550d1bfb9ac041f1936a578feb71f11e87f18
Jacob Pernell <jacob.pernell@gmail.com>
2017-11-14 01:35:21 -0800

    Trying to integrate and understand some code I found online. Not working yet, but at least it doesnt crash!

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,17 +4,74 @@
 	const inputText = document.querySelector('#input_text');
 	const inputTextButton = document.querySelector('#input_text_button');
 	const outputBox = document.querySelector('#output-box');
-	inputTextButton.addEventListener("click", stringifyInput);
+	inputTextButton.addEventListener("click", buttonPress);
 
-	function stringifyInput(event) {
+	function buttonPress(event) {
 		event.preventDefault();
+		outputBox.innerHTML = stringify(inputText);
+	}
+
+	function stringify(obj) {
+
+		if (obj === null) {
+			return "null";
+		}
+
+		if (obj === undefined || obj.constructor === Function) {
+			return;
+		}
+
+		if (obj.constructor === String) {
+			return `"${obj}"`;
+		}
+
+		if (obj.constructor === Array) {
+			if (obj.length) {
+				let partialJSON = [];
+
+				for (let i = 0; i < obj.length; i++) {
+					partialJSON.push(stringify(obj[i]));
+				}
+				return `[${partialJSON.join(",")}]`;
+			} else {
+				return '[]';
+			}
+		}
+
+		if (obj.constructor === Object) {
+			let keys = Object.keys(obj);
+			if (keys.length) {
+				let partialJSON = '';
+
+				for (let i = 0; i < keys.length; i++) {
+					let key = keys[i];
+
+					if (!key || obj[key] === undefined || typeof key === 'function' || typeof obj[key] === 'function') {
+
+					} else {
+						if (i === keys.length -1) {
+							partialJSON += stringify(key) + ":" + stringify(obj[key]);
+						} else {
+							partialJSON += stringify(key) + ":" + stringify(obj[key]) + ',';
+						}
+					}
+				}
+				return `{${partialJSON}}`;
+			} else {
+				return '{}';
+			}
+		}
+
 		let newString = "";
 
 		for (let i = 0; i < inputText.value.length; i++) {
 			newString += inputText.value.charAt(i);
 		}
-
-		outputBox.innerHTML = `Stringified value: ${newString}`;
 	}
+
+	let library = {
+		
+	};
+
 
 })();
